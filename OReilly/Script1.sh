2f2c3c4deb07be271339ecb91f9c5f8180cdee31
Raúl Ruiz <raulruiz@coca-cola.com>
2023-05-31 14:32:19 -0600

    OReilly Update Script
    Update on Scripts for OReilly

--- OReilly/Script1.sh
+++ OReilly/Script1.sh
@@ -1,8 +1,7 @@
-Id createdByUserId = '005G000000739rEIAQ';
-String env = 'UAT';
-datetime cratedDateValue = (datetime) date.newInstance(2023,5,25);
-date activationDateValue = date.newInstance(2040,12,31);
-String fakeEmail = 'Oreilly_'+ env +'_'+ System.now().format('yyyyMMdd') + '@yopmail.com';
+Id createdByUserId = '005G000000739rEIAQ'; //Validate
+String env = 'UAT'; //Update
+date activationDateValue = date.newInstance(2040,12,31); //Validate
+String fakeEmail = 'OR_'+ env +'_'+ System.now().format('yyyyMMdd') + '@yopmail.com';
 
 list<User> lstUsr=new List<User>();
 
@@ -12,23 +11,14 @@ dlo.EmailHeader.triggerAutoResponseEmail = false;
 dlo.EmailHeader.triggerOtherEmail = false;
 dlo.EmailHeader.triggerUserEmail = false;
 
-system.debug('Select id,Email,UserName,IsActive, CreatedDate, CreatedById, Activation_Date__c '+
-              'from user '+
-              'where CreatedById = \''+createdByUserId+ '\' '+
-              'and CreatedDate >= ' + cratedDateValue.format('yyyy-MM-dd') + 'T00:00:00.000+0000 '+
-              'and CreatedDate <=' + cratedDateValue.addDays(2).format('yyyy-MM-dd') + 'T00:00:00.000+0000 ' +
-              'and IsActive = false '+
-              'and Activation_Date__c =' + String.valueof(activationDateValue) );
-
+//Update last_N_Days
 for(User usr:[Select id,Email,UserName,IsActive, CreatedDate, CreatedById, Activation_Date__c
               from user
               where CreatedById =: createdByUserId
-              and CreatedDate <: cratedDateValue
-              and CreatedDate >: cratedDateValue.addDays(2)
-              and IsActive = false
+              and CreatedDate = LAST_N_DAYS:6        
               and Activation_Date__c =: activationDateValue]){
-	usr.Email = fakeEmail;
-	usr.isActive = True;
+    usr.Email = fakeEmail;
+    usr.isActive = True;
     lstUsr.add(usr);
 }
 
